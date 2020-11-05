54c5f572eb33bb85e3c33e4953142da3583a3a10
Vu Tien Khang <kvutien.yes@gmail.com>
2020-11-05 12:38:23 +0100

    Update Community.sol

--- Community.sol
+++ Community.sol
@@ -2,11 +2,13 @@
 */
 pragma solidity ^0.5.0;
 // import './HelperInstitution.sol';
+// import './HelpWallet.sol';
 
 contract Community {
     address public owner;
     mapping(address => bool) public personsInNeed; // today we use bool; will be IPFS hash in the future
     HelperInstitution[] public helperInstitutions;
+    HelpWallet[] public helpWallets;    // each person in need has a wallet, to be further analysed
 
     constructor () public {
         owner = msg.sender;
@@ -18,6 +20,8 @@ contract Community {
         personsInNeed[_personInNeed] = true;  // today we use 'true'; will be IPFS hash in the future
         HelperInstitution helperInstitution = new HelperInstitution(_personInNeed);
         // there will be a contract to associate a personInNeed with a helperInstitution
+        // a helperInstitution may help n persons in need; a person in need may receive aid from n institutions
         helperInstitutions.push(helperInstitution);
+        // do the same for helpWallets
     }
 }
