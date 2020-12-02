68b87764baeea552955889cdc0cb18e6c2004b21
kvutien <kvutien@MBP18VTK-2.fritz.box>
2020-12-02 18:10:10 +0100

    PepitoDisguiseFactory stuffing & overall design

--- Pepito/test/PepitoDisguiseFactoryTest.js
+++ Pepito/test/PepitoDisguiseFactoryTest.js
@@ -1,4 +1,4 @@
-contract('PepitoDisguise', async (accounts) => {
+contract('PepitoDisguiseFactory', async (accounts) => {
     it('Calling set(x) should set storedData in storage to x', async() => {
         let newValue = 2;
         let instance = await PepitoDisguise.deployed()
@@ -9,15 +9,11 @@ contract('PepitoDisguise', async (accounts) => {
         assert.equal(newValue, returnedValue, "The returned value should equal the new value.")
     })
 
-    it('Calling createDisguise(x) should do something', async() => {
-
+    it('Calling createPepitoDisguise(x) should do something', async() => {
+        //
     })
 
-    it('Calling readDisguise(x) should do something', async() => {
-
-    })
-
-    it('Calling updateDisguise(x) should do something', async() => {
-        
+    it('Calling getPepitoDisguises(x) should do something', async() => {
+        //
     })
 })
