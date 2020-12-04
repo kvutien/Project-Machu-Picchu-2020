472e9f53c3668e33a8323f6291b8d93d7027d427
kvutien <kvutien@MBP18VTK-2.fritz.box>
2020-12-04 14:07:31 +0100

    add Community and HelperInstitution is design

--- Pepito/contracts/PepitoDisguise.sol
+++ Pepito/contracts/PepitoDisguise.sol
@@ -5,10 +5,13 @@
 pragma solidity >=0.4.22 <0.8.0;
 
 contract PepitoDisguise {
+    /*
+    First example of a character, that can be copied to generate farmers, refugees, homeless etc.
+    */
     // state variables
-    uint public storedData;         // test data, to be removed
-    address pepitoDisguiseOwner
-    uint initialBalance
+    string public storedData;         // test data, to be replaced by a struct describing a dsguise
+    address pepitoDisguiseOwner;
+    uint initialBalance;
     
     constructor(address _pepitoDisguiseOwner, uint _initialBalance) public {
         pepitoDisguiseOwner = _pepitoDisguiseOwner;
@@ -16,9 +19,17 @@ contract PepitoDisguise {
     }
     
     // functions
-    function set(uint x) public {   // test function, to be removed
-        storedData = x;
+    function setTop(string memory x) public {   // test function, set one character feature in the struct
+        storedData = x;                     // replace by adequate
     }
+    function setClothe(string memory x) public {}
+    function setAccessories(string memory x) public {}
+    function setFacialHair(string memory x) public {}
+    function setEyes(string memory x) public {}
+    function setEyebrows(string memory x) public {}
+    function setMouth(string memory x) public {}
+    function setNose(string memory x) public {}
+    function setSkin(string memory x) public {}
 
     function createDisguise() public payable {
         // to be filled
