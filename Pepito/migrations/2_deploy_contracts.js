5e8105414b317ab74a0144d6ffe73ef17f5c4a30
kvutien <kvutien@MBP18VTK-2.fritz.box>
2020-12-05 20:31:53 +0100

    add PepitoTest

--- Pepito/migrations/2_deploy_contracts.js
+++ Pepito/migrations/2_deploy_contracts.js
@@ -1,5 +1,7 @@
 const PepitoDisguise = artifacts.require("PepitoDisguise");
+const Pepito = artifacts.require("Pepito");
 
 module.exports = function (deployer) {
   deployer.deploy(PepitoDisguise);
+  deployer.deploy(Pepito);
 };
