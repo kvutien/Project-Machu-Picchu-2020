ca584ec93d30deeac0fd7f478628af2e607d901c
kvutien <kvutien@MBP18VTK-2.fritz.box>
2020-12-05 20:27:10 +0100

    merge Pepito & PepitoDisguiseFactory

--- Pepito/contracts/Pepito.sol
+++ Pepito/contracts/Pepito.sol
@@ -3,11 +3,11 @@
     Demo dApp for Machu Picchu. Also Final Project of Vu Tien Khang
     Pepito is a Caribbian corsair. He can create PepitoDisguise using PepitoDisguiseFactory
     Pepito has a Circuit Breaker to stop creating disguises if Hernadez de La Banane discovers
-    Remix compiled successfully 2020-12-04
 */
 pragma solidity >=0.4.22 <0.8.0;
 
-import "./PepitoDisguiseFactory.sol";
+// import "./PepitoDisguiseFactory.sol";    // integrate PepitoDisguiseFactory in Pepito
+import "./PepitoDisguise.sol";
 
 
 contract Pepito {
@@ -15,10 +15,19 @@ contract Pepito {
         Pepito is the owner of the whole system
         He calls PepitoDisguiseFactory to create a disguise
         He has a circuit breaker to halt everything in case of serious unsolved bug
+        He is also a factory of disguises.
+        - the main interest of a factory is to maintain an array or mapping of addresses of child contracts
+        - specially useful for persons-in-need because their contract will be their virtual secretary
+            and will maintain their balance of tokens 
+            and will execute the few orders sent via SMS by the persons-in-need
+
+        Remix-compiled successfully 2020-12-05
     */
     bool public stopped;        // the circuit breaker
     address public owner;       // Pepito is the owner of the whole system
     uint256 public initialBalance;  // initial balance of a disguise
+    address[] public pepitoDisguiseAddresses;   // array of addresses of PepitoDisguise
+    event PepitoDisguiseCreated(address pepitoDisguise);
     
     constructor() public {
         stopped = false;
@@ -43,13 +52,27 @@ contract Pepito {
         stopped = !stopped;
     }
     
-    function registerDisguise(PepitoDisguiseFactory factory) stopInEmergency public payable {
+    function registerDisguise() stopInEmergency public payable {
         // stop creating new disguise if circuit breaker activated
-        factory.createPepitoDisguise (owner, initialBalance);
+        createPepitoDisguise();
     }
     
-    function withdraw () onlyInEmergency public payable {
+    function withdraw () onlyInEmergency public payable {   // change to external if never called inside contract
         // some code to withdraw all tokens from all disguises if circuit breaker activated
         // and is hopelessly FOOBAR
     }
+    
+    function createPepitoDisguise() public {
+        require (owner == msg.sender);             // the transaction caller must be _owner Pepito
+        require (initialBalance != uint256(0));    // initial balance must not be nil
+        PepitoDisguise pepitoDisguise = new PepitoDisguise(owner, initialBalance);
+        // disguise is a future virtual secretary of persons-in-need, so its address is useful
+        pepitoDisguiseAddresses.push(address(pepitoDisguise));   // record address of disguise
+        emit PepitoDisguiseCreated(address(pepitoDisguise));
+    }
+    
+    function getPepitoDisguises() external view returns (address[] memory) {
+        return pepitoDisguiseAddresses;
+    }
+
 }
