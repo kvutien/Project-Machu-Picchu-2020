f0825a430291996b58a006d0c3c7c9b82d861f7a
kvutien <kvutien@MBP18VTK-2.fritz.box>
2020-12-06 21:53:23 +0100

    add natSpec comments

--- Pepito/contracts/Pepito.sol
+++ Pepito/contracts/Pepito.sol
@@ -1,86 +1,89 @@
 // SPDX-License-Identifier: MIT
-/*
-    Demo dApp for Machu Picchu. Also Final Project of Vu Tien Khang
-    Pepito is a Caribbian corsair. He can create PepitoDisguise using PepitoDisguiseFactory
-    Pepito has a Circuit Breaker to stop creating disguises if Hernadez de La Banane discovers
-*/
 pragma solidity >=0.4.22 <0.8.0;
 
-// import "./PepitoDisguiseFactory.sol";    // integrate PepitoDisguiseFactory in Pepito
 import "./PepitoDisguise.sol";
 
+/**
+    @title  Demo dApp for Machu Picchu. Also Final Project of
+    @author Vu Tien Khang
+    @notice Pepito is a Caribbian corsair. He can create up to 512 PepitoDisguise
+    @dev    Pepito's Circuit Breaker stops creating disguises if Hernadez de La Banane discovers the trick
+    @dev    The circuit breaker is called to halt everything in case of serious unsolved contract exploit
+    @dev    He is also a factory of disguises.
+    @dev    - the main interest of a factory is to maintain an array or mapping of addresses of child contracts
+    @dev    - specially useful for persons-in-need because their contract will be their virtual secretary
+    @dev        and will maintain their balance of tokens 
+    @dev        and will execute the few orders sent via SMS by the persons-in-need
 
+    @dev    Remix-compiled successfully 2020-12-06
+*/
 contract Pepito {
-    /*
-        Pepito is the owner of the whole system
-        He calls PepitoDisguiseFactory to create a disguise
-        He has a circuit breaker to halt everything in case of serious unsolved bug
-        He is also a factory of disguises.
-        - the main interest of a factory is to maintain an array or mapping of addresses of child contracts
-        - specially useful for persons-in-need because their contract will be their virtual secretary
-            and will maintain their balance of tokens 
-            and will execute the few orders sent via SMS by the persons-in-need
-
-        Remix-compiled successfully 2020-12-05
-    */
-    bool public stopped;        // the circuit breaker
-    address public owner;       // Pepito is the owner of the whole system
-    uint256 public initialBalance;  // initial balance of a disguise
-    uint256 public disguiseNumber;  // number of disguises in array pepitoDisguiseAddresses
-    address[512] public pepitoDisguiseAddresses;   // array of addresses of PepitoDisguise
-    // array is used because disguises will be iterated and displayed
-    // mapping may be used when disguises are transposed into people-in-need
-    // for the demo we limit array size to 512; in real disguises will be in IPFS database
-    event PepitoDisguiseCreated(address pepitoDisguise);
+    bool public stopped;            /// @dev    the circuit breaker
+    address public owner;           /// @dev    account that deployed Pepito
+    uint256 public initialBalance;  /// @dev    initial balance of all disguises
+    uint256 public disguiseNumber;  /// @dev    running number of disguises in array pepitoDisguiseAddresses
+    address[512] public pepitoDisguises;    /// @dev    array of contracts pepitoDisguise
+    /// @dev    array is used because disguises will be iterated and displayed
+    /// @dev    mapping may be used when disguises are transposed into people-in-need that won't be iterated
+    /// @dev    for the demo, we limit array size to 512; in real, disguises will be in IPFS database w/o number limit
+    event PepitoDisguiseCreated(address aDisguise);
     
     constructor() public {
         stopped = false;
-        owner = msg.sender;     // the owner is the EOA that created Pepito
-        initialBalance = 10;    // initial balance is 10 Pepito tokens
-        disguiseNumber = 0;     // initial number of disguises created
+        owner = msg.sender;     /// @dev    the owner is the EOA that deployed Pepito
+        initialBalance = 10;    /// @dev    initial balance is 10 Pepito tokens
+        disguiseNumber = 0;     /// @dev    initial number of disguises created
     }
     
     modifier isAdmin() {
-        require(owner == msg.sender);
+        require(owner == msg.sender);   /// @dev    the caller of the function must be Pepito
         _;
     }
-    modifier stopInEmergency() {
+    modifier stopInEmergency() {    /// @dev    the caller of the breaker must be Pepito
         if(!stopped) _;
     }
-    modifier onlyInEmergency() {
+    modifier onlyInEmergency() {    /// @dev    the caller of the breaker must be Pepito
         if(stopped) _;
     }
 
     function toggleContractActive() isAdmin public {
-        // You can add an additional modifier that restricts stopping a contract to be
-        // based on another action, such as a vote of users
+        /// @dev    Circuit breaker to stop the smart contract in desperate cases & restart it
+        /// @dev    In the future we can add an additional modifier that restricts stopping a contract to be
+        /// @dev    based on another action, such as a vote of users
         stopped = !stopped;
     }
     
     function registerDisguise() stopInEmergency public payable {
-        // stop creating new disguise if circuit breaker activated
+        /// @dev    create a disguise and (future) record it in IPFS
+        /// @dev    stop all creation of new disguise if circuit breaker activated
         createPepitoDisguise();
     }
     
-    function withdraw () onlyInEmergency public payable {   // change to external if never called inside contract
-        // some code to withdraw all tokens from all disguises if circuit breaker activated
-        // and is hopelessly FOOBAR
-    }
+    function withdraw () onlyInEmergency public payable {
+        /// @dev    withdraw balances of all tokens when situation is desperate
+        /// @dev    change 'public' to 'external' to reduce gas if never called inside this contract
+        /// @dev    add some code to withdraw all tokens from all disguises if circuit breaker activated
+        /// @dev    and contract is hopelessly FOOBAR
+   }
     
     function createPepitoDisguise() public {
+        /// @dev    deploy an instance of PepitoDisguise with properties transferred from caller
         require (owner == msg.sender, "the transaction caller must be Pepito");
         require (initialBalance != uint256(0), "initial balance of disguise cannot be zero");
         require (disguiseNumber < 512, "there has been already 512 disguises created");
+
         PepitoDisguise pepitoDisguise = new PepitoDisguise(owner, initialBalance);
-        // disguise is a future virtual secretary of persons-in-need, so its address is useful
-        pepitoDisguiseAddresses[disguiseNumber] = address(pepitoDisguise);   // record address of disguise
+        /// @dev    disguise is a future virtual secretary of persons-in-need, so its contract address is useful
+        pepitoDisguises[disguiseNumber] = pepitoDisguise;   // record disguise smart contract
         disguiseNumber += 1;
         emit PepitoDisguiseCreated(address(pepitoDisguise));
     }
     
-    function getPepitoDisguises(uint i) external view returns(address) {
-        // this function will be called inb a JavaScript loop
+    function getPepitoDisguise(uint i) external view returns(address) {
+        /// @dev    this function will be called from a JavaScript loop
+        /// @dev    i is loop index, rank in the array of disguises
+        /// @return one instance of pepitoDisguiseAddress
         require (i < 512, "cannot exist more than 512 disguises");
-        return pepitoDisguiseAddresses[i];
+        return pepitoDisguises[i];
     }
 }
--- Pepito/contracts/PepitoDisguise.sol
+++ Pepito/contracts/PepitoDisguise.sol
@@ -5,44 +5,53 @@
 */
 pragma solidity >=0.4.22 <0.8.0;
 
+/** Solidity NatSpec format
+    @title  Demo dApp for Machu Picchu. Also Final Project of
+    @author Vu Tien Khang
+    @notice Pepito is a Caribbian corsair. He can create up to 512 PepitoDisguise
+    @notice A disguise is composed of many features
+    @notice Copy this contract to generate also farmers, refugees, homeless etc.
+
+    @dev    Remix-compiled successfully 2020-12-06
+*/
 contract PepitoDisguise {
-    /*
-        A disguise of Pepito is composed of many features
-        Copy this contract to generate also persons-in-need (farmers, refugees, homeless etc.)
-    */
     // state variables
-    string public storedData;           // test data, to be replaced by a struct describing a disguise
-    // address pepitoDisguiseOwner;     // check if really needed?
-    uint balance;                       // will be used for person-in-need, not for disguise
+    string public storedData;   /// @dev    test data, to be replaced by a struct describing a disguise
+    address pepitoDisguiseOwner;/// @dev    the owner of the disguise is Pepito
+    uint balance;               /// @dev    running balance of pepitoTokens of this disguise
+    /// @dev    when transposed to person-in-need, balance will be a mapping of tokens from many helperInstitutions
     
     constructor(address _pepitoDisguiseOwner, uint _initialBalance) public {
-        // pepitoDisguiseOwner = _pepitoDisguiseOwner;  // check if really needed?
-        balance = _initialBalance;      // in future versions, balance will be a mapping of tokens of various names
+        pepitoDisguiseOwner = _pepitoDisguiseOwner;
+        balance = _initialBalance;
     }
     
-    // functions
-    function setTop(string memory x) public {   // test function, set one character feature in the struct
-        storedData = x;                         // replace by adequate
+    /// @dev    functions to set elements of the struct storedData with data of the disguise
+    function setTop(string memory x) public {   /// @dev    test function, set one character feature in the struct
+        storedData = x;                         /// @dev    to be replaced by adequate when finalised
     }
-    function setClothe(string memory x) public {}
-    function setAccessories(string memory x) public {}
-    function setFacialHair(string memory x) public {}
-    function setEyes(string memory x) public {}
-    function setEyebrows(string memory x) public {}
-    function setMouth(string memory x) public {}
-    function setNose(string memory x) public {}
-    function setSkin(string memory x) public {}
+    function setClothe(/*string memory x*/) public {}
+    function setAccessories(/*string memory x*/) public {}
+    function setFacialHair(/*string memory x*/) public {}
+    function setEyes(/*string memory x*/) public {}
+    function setEyebrows(/*string memory x*/) public {}
+    function setMouth(/*string memory x*/) public {}
+    function setNose(/*string memory x*/) public {}
+    function setSkin(/*string memory x*/) public {}
 
     function createDisguise() public payable {
-        // to be filled
+        /// @dev    write the struct storedData in IPFS database
+        /// @dev    to be filled
     }
 
     function readDisguise() public {
-        // to be filled
+        /// @dev    retrieves the struct storedData from IPFS database
+        /// @dev    to be filled
     }
 
     function updateDisguise() public payable {
-        // to be filled
+        /// @dev    read, update, write back the struct storedData in IPFS database
+        /// @dev    to be filled
     }
     
 }
