1007a331aa3435e8e379ef733ee63fe782aca78d
kvutien <kvutien@MBP18VTK-2.fritz.box>
2020-12-06 08:48:20 +0100

    optimise global variables in PepitoDisguise

--- Pepito/contracts/PepitoDisguise.sol
+++ Pepito/contracts/PepitoDisguise.sol
@@ -7,22 +7,22 @@ pragma solidity >=0.4.22 <0.8.0;
 
 contract PepitoDisguise {
     /*
-        A disguise is composed of many features
-        Copy this contract to generate also farmers, refugees, homeless etc.
+        A disguise of Pepito is composed of many features
+        Copy this contract to generate also persons-in-need (farmers, refugees, homeless etc.)
     */
     // state variables
-    string public storedData;         // test data, to be replaced by a struct describing a dsguise
-    address pepitoDisguiseOwner;
-    uint initialBalance;
+    string public storedData;           // test data, to be replaced by a struct describing a disguise
+    // address pepitoDisguiseOwner;     // check if really needed?
+    uint balance;                       // used only for person-in-need
     
     constructor(address _pepitoDisguiseOwner, uint _initialBalance) public {
-        pepitoDisguiseOwner = _pepitoDisguiseOwner;
-        initialBalance = _initialBalance;
+        // pepitoDisguiseOwner = _pepitoDisguiseOwner;
+        balance = _initialBalance;
     }
     
     // functions
     function setTop(string memory x) public {   // test function, set one character feature in the struct
-        storedData = x;                     // replace by adequate
+        storedData = x;                         // replace by adequate
     }
     function setClothe(string memory x) public {}
     function setAccessories(string memory x) public {}
